7f5fabcb52c24eb90009d1dc33ddbcd06f38f34e
douglas-cerrato <douglascerrato2@gmail.com>
2024-03-03 05:31:02 +0000

    Still Working on Backend of signup.php Currently have the backend where it checks if every box is filled out with data needed to get passed into our database. Also have it where we check if both the password and verify password box match so we can hash the password if the user put it in correctly. Next to work on is hashing the password, and adding all the information to our database to be accessed as a logged in user.

--- signup.php
+++ signup.php
@@ -2,11 +2,53 @@
    require_once('db_connection.php');
 
    if ($_SERVER["REQUEST_METHOD"] == "POST"){
+      // Check if First Name box is empty, if not then declare variable
       if (!empty($_POST['fname_textbox'])){
-         // If FirstName text box isn't empty, declare it's variable
          $fname = $_POST['fname_textbox'];
       } else{
          die("First name is required!!!");
+      }
+      
+      // Check if Last Name box is empty, if not then declare variable
+      if (!empty($_POST['lname_textbox'])){
+         $lname = $_POST['lname_textbox'];
+      } else{
+         die("Last name is required!!!");
+      }
+
+      // Check if Email box is empty, if not then declare variable
+      if (!empty($_POST['email_textbox'])){
+         //TODO: late Check if this email is a valid email
+         $email = $_POST['email_textbox'];
+      } else{
+         die("Email is required!!!");
+      }
+
+      // Check if Password box is empty, if not then declare variable
+      if (!empty($_POST['password1_box'])){
+         $passwd = $_POST['password1_box'];
+         $passwd1 = true;
+      } else{
+         $passwd1 = false;
+         die("Password is required");
+      }
+
+      // Check if Password Verification box is empty, if not then declare variable
+      if (!empty($_POST['verify_password'])){
+         $verify_passwd = $_POST['verify_password'];
+         $passwd2 = true;
+      } else{
+         $passwd2 = false;
+         die("Verify Password is required!!!");
+      }
+   }
+
+   // Checks if both password boxes were filled
+   if ($passwd1 == true && $passwd2 == true){
+      if ($passwd == $verify_passwd){
+         //TODO: Hash password and save it to the database
+      } else{
+         die("Passwords don't match: $passwd and $verify_passwd");
       }
    }
 
@@ -34,7 +76,7 @@
          <div>
             <label for="email">Enter Email:</label>
             <input type="text" id="email" name="email_textbox"></input>
-         <div>
+         </div>
 
          <div>
             <label for="psswd">Enter Password:</label>
