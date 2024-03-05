d6616de85f23e2571f3861e8d82ec1835248b86d
douglas-cerrato <douglascerrato2@gmail.com>
2024-03-05 07:08:53 +0000

    Added logout.php File used to kill session on logout and redirect back to home page

---
+++ logout.php
@@ -0,0 +1,11 @@
+<?php
+    // Start session
+    session_start();
+
+    // Destroy the session
+    session_destroy();
+
+    // Redirect to home page
+    header("Location: home.php");
+    exit();
+?>
