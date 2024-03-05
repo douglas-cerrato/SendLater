6912c8a5edb396f0197072c51921965b4bf037c5
douglas-cerrato <douglascerrato2@gmail.com>
2024-03-05 13:42:00 +0000

    Rename home.php to index.php

---
+++ index.php
@@ -0,0 +1,186 @@
+<!DOCTYPE html>
+<html><head>
+    <title>SendLater</title>
+    <style>
+        body {
+            margin: 20px;
+            padding: 0;
+            background-color: #0E4D92;
+        }
+        .container {
+	    display: flex;
+	    flex-direction: column;
+	    align-items: flex-start;
+            max-width: 800px;
+            margin: 0 auto;
+            /*! min-height: 90vh; */
+            padding: 20px;
+            background-color: #C0C0C0;
+            border-radius: 5px;
+            box-shadow: 0 2px 4px rgba(0, 0, 0, 0.1);
+        }
+        h1 {
+            font-family: fantasy, Papyrus;
+            text-align: center;
+            font-size: 52px;
+            width: auto;
+            align-items: center;
+            margin-left: 250px;
+            margin-right: auto;
+            margin-bottom: auto;
+            display: flex;
+        }
+        h2 {
+            margin-top: 24%;
+            text-align: left;
+            font-size: 36px;
+            padding-left: 20px;
+            font-family: fantasy, Papyrus;
+        }
+	h3 {
+            margin-top: 8%;
+	    margin-bottom: -25px;
+            text-align: left;
+            font-size: 36px;
+            padding-left: 20px;
+            font-family: fantasy, Papyrus;
+        }
+	h4 {
+	    text-align: center;
+	    font-size: 12px;
+	}
+	.buttons {
+	    display: flex;
+    	    justify-content: flex-start;
+    	    width: 100%;
+    	    margin-bottom: 0;
+	        padding-right: 10px;
+            margin-top: 50px;
+             
+        }
+        .btn {
+            display: inline-block;
+            padding: 15px 20px;
+            margin-left: 7%;
+            margin-bottom: 3px;
+            /*! margin-right: 2%; */
+            background-color: #0E4D92;
+            color: #fff;
+            text-decoration: none;
+            border-radius: 5px;
+            text-align: center;
+        }
+        .btn:hover {
+            padding: 15px 20px;
+            margin-left: 7%;
+            margin-bottom: 3px;
+            margin-right: 2%;
+            background-color: #E97451;
+            color: #fff;
+            text-decoration: underline;
+            border-radius: 5px;
+            text-align: center;
+        }
+        ul {
+            list-style-type: disclosure-closed;
+            padding: 5%;
+            font-size: 20px;
+        }
+        li {
+            padding: 5px 0;
+        }
+	.sidebar {
+	    width: 200px;
+	    background-color: #f0f0f0;
+	    padding: 20px;
+	    border-radius: 5px;
+	    box-shadow: 0 2px 4px rgba(0, 0, 0, 0.1);
+	    /*! margin-right: 20px; */
+        position: relative;
+        top: unset;
+        right: 20px;
+	    /*! align-items: end; */
+	    /*! display: flex; */
+	}
+    .sidebar h2 {
+        text-align: center;
+        pointer-events: absolute;
+        top: 0;
+        left: 0;
+        right: 0;
+        margin: 0;
+        padding: 0 0;
+    }
+	.advertisement {
+	    /*! background-color: #007bff; */
+	    /*! color: #fff; */
+	    /*! padding: 20px; */
+	    /*! border-radius: 5px; */
+	    /*! text-align: center; */
+        /*! margin-top: 20px; */
+	}
+    .sidebar p:not(:last-child) {
+    text-align: start;
+    font-weight: normal;
+    font-size: 18px;
+    }
+    .sidebar p:last-child {
+    text-align: center;
+    text-decoration: colorChange 2s infinite;
+    font-weight: bold;
+    font-size: xx-large;
+    }
+    @keyframes colorChange {
+        0% {color:green;}
+        50% {color:red;}
+        100% {color: green;}
+    }
+    .flex-container {
+        display: flex;
+        justify-content: space-around;
+    }
+    
+    </style>
+</head>
+<body>
+    <div class="container">
+        <div class="flex-container">
+            <div>
+        <h1 style="margin-top: 10px;margin-right: auto;height: 81.9px;width: 1px;">SendLater</h1>
+        <a href="register.php" class="btn" style="/*! padding-top: 015px; */width: 55px;padding-left: 20px;position: absolute;/*! margin-right: auto; */margin-left: auto;">Sign Up</a>
+        <a href="login.php" class="btn" style="width: 52px;margin-right: auto;position: absolute;padding-left: 20px;margin-left: 11%;">Login</a>
+        <h2 style="margin-bottom: -25px;">Features</h2>
+        <ul>
+            <li>Send emails to anyone you desire.</li>
+            <li>Emails can be scheduled to send at any desired date and time.</li>
+            <li>Send as many emails as you would like.</li>
+            <li>Reset your account password if you would like to change it.</li>
+        </ul>
+	<h3>Coming Soon in 1.2.0</h3>
+	<ul>
+	    <li>Add attachments to emails.</li>
+        <li>Schedule reoccurring emails (daily, weekly and monthly).</li>
+        <li>Manage and edit scheduled emails.</li>
+	    <li>Additional features to personalize your profile.</li>
+	    <li>Store multiple signatures for any occasion.</li>
+	    <li>Error free emails with spelling, grammar, and syntax errors.</li>
+	</ul>	    
+    </div>
+	<div class="sidebar" style="top: 200px;padding-top: 3%;padding-right: 4%;padding-left: 4%;right: -15%;/*! bottom: 0px; */margin-bottom: 35%;">
+	<h4>SUPPORTED BY:</h4>
+  <h2>AutoSource</h2>
+	 
+  
+  <p style="margin-bottom: 0px;margin-top: 30px;">New factory replacement parts.</p>Huge Selection of performance and aftermarket parts.<p style="margin-top: 0;margin-bottom: 0;"></p><p style="margin-bottom: 0px;margin-top: 0px;">Thousands of parts in store and ready to ship.</p>Find the best deals on all parts and accessories for your car!<p style="margin-top: 35px;">FREE SHIPPING!! with PROMO CODE: "CMPS3420"
+</p>
+  
+  
+  
+  
+
+
+</div>
+        </div>
+    <div class="advertisement">
+    </div>
+            </div></body></html>
